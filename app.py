c627cd2c133fbe52b82b4a8af7717c110f8d3a18
Ryan Aday <ryanobelisk@gmail.com>
2019-03-28 10:58:09 -0400

    added templates

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)  # create instance of class Flask
 # root function for index.html
 @app.route("/", methods=['GET','POST'])
 def root():
-
+    return render_template("home.html")
 # run app
 if __name__ == "__main__":
     app.debug = True

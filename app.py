92fbbe2b9dd272bba361b8d22d84a2ac11a57e48
Ryan Aday <ryanobelisk@gmail.com>
2019-03-28 10:50:47 -0400

    adding app.py template

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)  # create instance of class Flask
+
+# root function for index.html
+@app.route("/", methods=['GET','POST'])
+def root():
+
+# run app
+if __name__ == "__main__":
+    app.debug = True
+    app.run()

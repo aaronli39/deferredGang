119b06acb97a4b2873596cb8b90a6af0c8f8fec9
raunakchowdhury <rchowdhury5@stuy.edu>
2019-04-01 15:11:55 -0400

    updated json properly

--- static/index.js
+++ static/index.js
@@ -2,8 +2,9 @@
 // Source: https://github.com/datasets/sea-level-rise
 var data = [];
 
-var colleges = require('../data/colleges.json');
-console.log(colleges['id']);
+d3.json('https://raw.githubusercontent.com/aaronli39/deferredGang/master/data/colleges.json').then(function(data){
+  console.log(data);
+});
 
 var years = [1993, 1994, 1995, 1996, 1997, 1998, 1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014];
 var values = [6.297493046, 6.310933553, 6.452568661, 6.62763131, 6.733920367, 6.865806069, 6.848689771, 6.920582014, 7.087460011,
@@ -14,9 +15,9 @@ for (var index = 0; index < years.length; index++){
   data[index] = [years[index], values[index]];
 };
 
-d3.json("https://api.datausa.io/api/?sort=desc&show=university&required=oos_tuition%2Cstate_tuition&sumlevel=all&year=all").then(function(data) {
-  console.log(data['data']['']);
-});
+// d3.json("https://api.datausa.io/api/?sort=desc&show=university&required=oos_tuition%2Cstate_tuition&sumlevel=all&year=all").then(function(data) {
+//   console.log(data['data']['']);
+// });
 
 var width = 840,
     height = 300;

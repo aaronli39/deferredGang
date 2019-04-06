fffb32740e8f45b6a6489ee4945063b6fb7c44a4
raunakchowdhury <rchowdhury5@stuy.edu>
2019-04-06 19:06:02 -0400

    added files

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ from flask import Flask, render_template
 app = Flask(__name__)  # create instance of class Flask
 
 # root function for index.html
-@app.route("/", methods=['GET','POST'])
-def root():
+@app.route("/<college>", methods=['GET','POST'])
+def root(college):
     return render_template("home.html")
 # run app
 if __name__ == "__main__":

c4b7e43c161dfaa064a2ae8c240ad384a6cad37b
aaronli39 <aaronli39@gmail.com>
2019-04-07 22:38:17 -0400

    fixed app

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ from flask import Flask, render_template
 app = Flask(__name__)  # create instance of class Flask
 
 # root function for index.html
-@app.route("/<college>", methods=['GET','POST'])
-def root(college):
+@app.route("/")
+def root():
     return render_template("home.html")
 # run app
 if __name__ == "__main__":

cf75003915390ad08723cd4057c4f50b0c8400fa
sxia1 <sxia1@stuy.edu>
2019-04-08 13:49:47 -0400

    integration attempt

--- static/search.js
+++ static/search.js
@@ -1,30 +1,3 @@
-function get_tuition(id){
-    d3.json("https://api.datausa.io/api/?sort=desc&show=university&required=oos_tuition%2Cstate_tuition&sumlevel=all&year=all").then(function(data){
-	var tuition_array = data['data'];
-	for(i = 0; i < tuition_array.length; i ++){
-	    if(tuition_array[i][0] == '2016' &&
-	       tuition_array[i][1] == id){
-		console.log(tuition_array[i][2]);
-		//INSERT FUNCTION
-	    }
-	}
-    });
-};
-
-function get_accept(id){
-    d3.json("https://api.datausa.io/api/?sort=desc&show=university&required=applicants_total%2Cadmissions_total&sumlevel=all&year=all").then(function(data){
-	var accept_array = data['data'];
-	for(i = 0; i < accept_array.length; i ++){
-	    if(accept_array[i][0] == '2016' &&
-	       accept_array[i][1] == id){
-		console.log(100*accept_array[i][3]/accept_array[i][2]);
-		//INSERT FUNCTION
-	    }
-	}
-    });
-};
-
-
 var submitB = document.getElementById("submit");
 console.log(submitB);
 submitB.addEventListener('click', function(){
@@ -41,9 +14,7 @@ submitB.addEventListener('click', function(){
 	    if(college_names[i].toUpperCase() == filter){
 		here = true;
 		var collegeName = college_names[i];
-		var collegeId = colleges['names'][collegeName]
-		get_tuition(collegeId);
-		get_accept(collegeId);
+		generate_graphs(collegeName);
 	    }
 	}
 	//Print message if college doesn't exist

666cfad020356b4145a7d032d3e757c81eb84a35
raunakchowdhury <rchowdhury5@stuy.edu>
2019-04-07 23:55:51 -0400

    bar graph workingg

--- static/bar.js
+++ static/bar.js
@@ -23,172 +23,80 @@ var tuition_bar = function(colleges){
     d3.json(url_choice).then(function(data) {
       var array = data['data'];
       // will hold bar graph data
-      var bar_data = [{},{},{},{}];
-
-      // toss in each unviersity into its own directory
-      for (var uni_index = 0; uni_index < selected_universities.length; uni_index++){
-        bar_data[uni_index]['name'] = selected_universities[uni_index];
-      };
+      var bar_data = [
+        {'year': 2013, 'values': []},
+        {'year': 2014, 'values': []},
+        {'year': 2015, 'values': []},
+        {'year': 2016, 'values': []},
+      ];
 
       var tuition_list = []; //for collecting all tuition
 
+      // Generates data in the form: {year: <y>, 0: <1st college tuition>, 1: <2nd college tuition>...}
       for (var index = 0; index < array.length; index++){
         // extract the appropriate entry
         var college = array[index];
-        selected_universities.forEach(function(university){
+        for (var uni_index = 0; uni_index < selected_universities.length; uni_index++){
+          var university = selected_universities[uni_index];
           // check for each college
           if ( parseInt(colleges['names'][university]) == college[1]){
             // create an entry in the appropriate slot
-            // bar_data[college[0] - 2013][college[0]] = college[2];
-            for (var data_index = 0; data_index < bar_data.length; data_index++){
-              if (bar_data[data_index]['name'] == university){
-                bar_data[data_index][college[0]] = college[2];
-              };
-            };
+            bar_data[college[0] - 2013]['values'].push( {
+              uni_index:college[2],
+              'name': university,
+            });
             tuition_list.push(college[2]);
           };
-        });
+        };
       };
 
-    // console.log(bar_data);
+    var years = [2013, 2014, 2015, 2016];
+    console.log(bar_data);
+
     //set domains
     var x = d3.scaleLinear()
-      .domain([2013,2016])
+      .domain(years)
       .range([20, width - margin['right'] * 2]);
 
-    var y = d3.scaleLinear()
+    var heightScale = d3.scaleLinear()
       .domain([0, d3.max(tuition_list)]) //d3.min(tuition_list)
-      .range([0, height - margin['top'] * 2]);
+      .range([height - margin['bottom'] * 2, margin['top']]);
 
-    var bars = chart.selectAll('g')
-      .data(bar_data)
-      .enter();
-
-    //split bars into equal lengths
+    //split sections into equal lengths
     var bandScale = d3.scaleBand()
-      .domain()
-      .padding(0.1)
+      .domain(years)
+      .rangeRound([20, width - margin['right'] * 2])
+      .padding(0.1);
+    // console.log(bandScale(2013))
 
+    //split bars in those sections into equal lengths
+    var barScale = d3.scaleBand()
+      .domain(selected_universities)
+      .range([0, bandScale.bandwidth()])
+      .padding(0.1);
+    // console.log(barScale.bandwidth());
 
     var color = d3.scaleOrdinal()
       .domain(selected_universities)
-      .range(["#6b486b", "#a05d56", "#d0743c", "#ff8c00"])
+      .range(["#C0C0C0", "#C0C0C0", "	#C0C0C0", "#6b486b"])
 
-    //apply rects using a loop
-    var years = [2013, 2014, 2015, 2016];
-    for(var index = 0; index < years.length; index++){
-      bars.append('rect')
-        .attr('class', 'bar' + String(index))
-        .attr('x', x(years[index]))
-        .attr('year', years[index])
-        .attr('y', function(d){
-          //input tuition data for that year
-          // console.log(d[years[index]])
-          return y(d[years[index]]);
+    chart.append('svg')
+      .selectAll('g')
+      .data(bar_data)
+      .join('g')
+        .attr('transform', function(d,i){
+          // console.log(d, d[years[i]]);
+          return 'translate(' + bandScale(years[i]) + ',0)';
         })
-        .attr('height', function(d) {
-          return y(d[years[index]]);
-        })
-        .attr('width', 20)
-        .attr('fill', function(d){
-          return color(d['name']);
-        });
-
-        // bars.append('rect')
-        //   .attr('class', 'bar' + String(index))
-        //   .attr('x', x(years[index]))
-        //   .attr('year', years[index])
-        //   .attr('y', function(d){
-        //     //input tuition data for that year
-        //     // console.log(d[years[index]])
-        //     return y(d[years[index]]);
-        //   })
-        //   .attr('height', function(d) {
-        //     return y(d[years[index]]);
-        //   })
-        //   .attr('width', 20)
-        //   .attr('fill', function(d){
-        //     return color(d['name']);
-        //   });
-
-    };
+      .selectAll('rect')
+      .data(function(d){ return d.values; })
+      .join('rect')
+        .attr('class', function(d,i){ return 'bar' + String(i); })
+        .attr('x', function(d){ return barScale(d['name']); })
+        .attr('y', function(d,i){ return heightScale(d.uni_index); })
+        .attr('tuition', function(d,i){ return d.uni_index; })
+        .attr('height', function(d,i) { return height - heightScale(d.uni_index); })
+        .attr('width', barScale.bandwidth())
+        .attr('fill', function(d){ return color(d.name); });
   });
 };
-
-// var years = [1993, 1994, 1995, 1996, 1997, 1998, 1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014];
-// var values = [6.297493046, 6.310933553, 6.452568661, 6.62763131, 6.733920367, 6.865806069, 6.848689771, 6.920582014, 7.087460011,
-//           7.319697525, 7.488352718, 7.507932839, 7.644563144, 7.697420009, 7.70214715, 7.90736541, 8.04635409, 8.122972567,
-//           8.053065004, 8.457057629, 8.546648227, 8.663700393];
-//
-// for (var index = 0; index < years.length; index++){
-//   data[index] = [years[index], values[index]];
-// };
-//
-//
-//
-// var x = d3.scaleLinear()
-//     .domain([d3.min(years) - 1, d3.max(years) + 1])
-//     .range([20, width - 40]);
-//
-// var y = d3.scaleLinear()
-//     .domain([d3.min(values) - 1, d3.max(values) + 1])
-//     .range([0, height]);
-//
-// var chart = d3.select(".chart")
-//     .attr("width", width)
-//     .attr("height", height);
-//
-// var plot = chart.selectAll("g")
-//     .data(data)
-//   .enter().append("g")
-//     .attr("transform", function(d) {
-//       return 'translate(' + x(d[0]) + ',' + (height - y(d[1])) + ')';
-//      })
-//
-// plot.append('rect')
-//   .attr('width', 4)
-//   .attr('height', 4);
-//
-// var x_scale = d3.scaleLinear()
-//   .domain([d3.min(years), d3.max(years)])
-//   .range([20,width-40]);
-//
-// var y_scale = d3.scaleLinear()
-//   .domain([d3.min(values), d3.max(values)])
-//   .range([height, 0]);
-//
-// var x_axis = d3.axisBottom()
-//   .scale(x_scale); //this be a fxn
-//   // .orient('left');
-//
-// var y_axis = d3.axisLeft()
-//   .scale(y_scale);
-//
-// chart.append('g')
-//   .attr('transform', 'translate(30, ' + (height - 25) +  ')')
-//   .call(x_axis);
-//
-// chart.append('g')
-//   .attr('transform', 'translate(50,0)')
-//   .call(y_axis);
-//
-// // x-axis label
-// chart.append('text')
-//   .attr('x', width / 2)
-//   .attr('y', height)
-//   .text('Year');
-//
-// // y-axis label
-// chart.append('text')
-//   .attr('transform', 'rotate(-90)') //causes all coordinates to be negated
-//   .attr('x', 0 - (height / 2))
-//   .attr('y', 0)
-//   .attr('dy', '1em')
-//   .style('text-anchor', 'middle')
-//   .text('NOAA - Adjusted sea level (inches)');
-//
-// // title
-// chart.append('text')
-//   .attr('x', width / 2 + 60)
-//   .attr('y', 10)
-//   .text('Figure 1. Global Average Sea Levels, 1993-2014')
